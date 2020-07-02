ed0c40311617a019e252af068b9e0a626a12d659
manmaha <manish.p.mahajan@gmail.com>
2020-07-02 20:08:15 +0800

    Update csi_camera.py

--- jetcam/csi_camera.py
+++ jetcam/csi_camera.py
@@ -12,6 +12,7 @@ class CSICamera(Camera):
     capture_fps = traitlets.Integer(default_value=30)
     capture_width = traitlets.Integer(default_value=640)
     capture_height = traitlets.Integer(default_value=480)
+    capture_flip = traitlets.Integer(default_value=2)
     
     def __init__(self, *args, **kwargs):
         super(CSICamera, self).__init__(*args, **kwargs)
@@ -29,8 +30,8 @@ class CSICamera(Camera):
         atexit.register(self.cap.release)
                 
     def _gst_str(self):
-        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
-                self.capture_device, self.capture_width, self.capture_height, self.capture_fps, self.width, self.height)
+        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=%d! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
+                self.capture_device, self.capture_width, self.capture_height, self.capture_fps, self.capture_flip, self.width, self.height)
     
     def _read(self):
         re, image = self.cap.read()
